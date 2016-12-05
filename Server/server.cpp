8eeb23cc3fe3535319d76f5a5a14e8719973cc70
Jim Viebke <jviebke@gmail.com>
2016-12-05 16:44:24 -0500

    a whole bunch of server code

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,6 +1,9 @@
 
 #include "server.h"
 
+#include <sstream>
+#include <iterator>
+
 using namespace pipedat;
 
 Server::Server()
@@ -19,28 +22,33 @@ void Server::run()
 	for (;;)
 	{
 		// get the next message
-		const Message message = input_queue.get();
+		Message message = input_queue.get();
+
+		// check if the user has entered a special command
+		if (message.data[0] == '/')
+		{
+			std::stringstream ss(message.data);
+			const std::istream_iterator<std::string> begin(ss);
+			std::vector<std::string> strings(begin, std::istream_iterator<std::string>());
+
+			if (strings.size() > 0)
+			{
+				std::lock_guard<std::mutex> lock(users_mutex);
+				handle_commands(users[message.id].connection, strings);
+			}
+
+			continue;
+		}
 
 		// get the user's room
-		std::string room_id;
+		std::string room_name;
 		{
 			std::lock_guard<std::mutex> lock(users_mutex);
-			room_id = users[message.id].second;
+			room_name = users[message.id].room_name;
 		}
 
-		// get the users in the room
-		std::set<pipedat::ConnectionID> users_in_room;
-		{
-			std::lock_guard<std::mutex> lock(room_mutex);
-			users_in_room = rooms[room_id];
-		}
-
-		// for each user in the room
-		for (const auto & user : users_in_room)
-			// if the user is not the sender
-			if (user != message.id)
-				// send a new message
-				output_queue.put(Message(user, message.data));
+		// forward their message to the room
+		send_to_room(room_name, message.data, message.id);
 	}
 }
 
@@ -56,17 +64,14 @@ void Server::listen_for_new_users()
 		// save the user
 		{
 			std::lock_guard<std::mutex> lock(users_mutex);
-			users[connection->get_id()] = make_pair(connection, "main");
+			users[connection->get_id()] = User_Info(connection, "User " + connection->get_id(), "main");
 		}
 
+		// add the user to the main room
 		{
 			std::lock_guard<std::mutex> lock(room_mutex);
-
-			// make sure the main room exists
-			if (rooms.find("main") == rooms.cend())
+			if (rooms.find("main") == rooms.cend()) // if the main room hasn't been created yet
 				rooms["main"] = std::set<pipedat::ConnectionID>();
-
-			// add the user to the main room
 			rooms["main"].insert(connection->get_id());
 		}
 
@@ -82,7 +87,7 @@ void Server::receive(connection_ptr connection)
 	{
 		const std::string data = connection->receive();
 
-		if (data.size() == 0) return;
+		if (data.size() == 0) return; // 0 indicates connection termination
 
 		input_queue.put(Message(connection->get_id(), data));
 	}
@@ -95,6 +100,62 @@ void Server::send()
 		// get the next message on the queue
 		const Message message = output_queue.get();
 		// send the message
-		users[message.id].first->send(message.data);
+		users[message.id].connection->send(message.data);
 	}
+}
+
+void Server::handle_commands(connection_ptr connection, const std::vector<std::string> & commands)
+{
+	// the caller locks the users_mutex
+
+	auto user_it = users[connection->get_id()];
+
+	if (commands[0] == "/name")
+	{
+		if (commands.size() < 2) return;
+
+		for (const auto & user : users)
+		{
+			if (user.second.user_name == commands[1])
+			{
+				output_queue.put(Message(connection->get_id(), commands[1] + " is already in use."));
+				return;
+			}
+		}
+
+		// tell the room that the user has changed their name
+		send_to_room(user_it.room_name, user_it.user_name + " has changed their name to " + commands[1], user_it.connection->get_id());
+
+		// change the user's name
+		user_it.user_name = commands[1];
+	}
+	else if (commands[0] == "/join")
+	{
+
+	}
+	else if (commands[0] == "/exit")
+	{
+
+	}
+	else if (commands[0] == "/help")
+	{
+
+	}
+}
+
+void Server::send_to_room(const std::string & room_name, const std::string & data, const ConnectionID & exclude)
+{
+	// get the users in the room
+	std::set<pipedat::ConnectionID> users_in_room;
+	{
+		std::lock_guard<std::mutex> lock(room_mutex);
+		users_in_room = rooms[room_name];
+	}
+
+	// for each user in the room
+	for (const ConnectionID & user : users_in_room)
+		// if the user is not the sender
+		if (user != exclude)
+			// send a new message
+			output_queue.put(Message(user, data));
 }
--- Server/server.h
+++ Server/server.h
@@ -21,11 +21,21 @@ public:
 class Server
 {
 private:
+
 	using connection_ptr = std::shared_ptr<pipedat::Connection>;
-	using user_data = std::pair<connection_ptr, std::string>;
+
+	class User_Info
+	{
+	public:
+		connection_ptr connection;
+		std::string user_name, room_name;
+		User_Info() {}
+		User_Info(connection_ptr set_connection, const std::string & set_name, const std::string & set_room)
+			: connection(set_connection), user_name(set_name), room_name(set_room) {}
+	};
 
 	// map a username to a Connection and a room name
-	std::map<pipedat::ConnectionID, user_data> users;
+	std::map<pipedat::ConnectionID, User_Info> users;
 	std::mutex users_mutex;
 
 	// map a room to its users
@@ -45,4 +55,8 @@ private:
 	void receive(connection_ptr connection);
 
 	void send();
+
+	void handle_commands(connection_ptr connection, const std::vector<std::string> & commands);
+
+	void send_to_room(const std::string & room_name, const std::string & data, const pipedat::ConnectionID & exclude);
 };
