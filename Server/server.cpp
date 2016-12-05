bbee9130cdff915a2dba18b2429517f58f6aeb68
Jim Viebke <jviebke@gmail.com>
2016-12-05 12:46:16 -0500

    do most of the server things

--- Server/server.cpp
+++ Server/server.cpp
@@ -10,7 +10,38 @@ Server::Server()
 
 void Server::run()
 {
+	// start the listening thread
 	std::thread(&Server::listen_for_new_users, this).detach();
+
+	// start the receiving thread
+	std::thread(&Server::send, this).detach();
+
+	for (;;)
+	{
+		// get the next message
+		const Message message = input_queue.get();
+
+		// get the user's room
+		std::string room_id;
+		{
+			std::lock_guard<std::mutex> lock(users_mutex);
+			room_id = users[message.id].second;
+		}
+
+		// get the users in the room
+		std::set<pipedat::ConnectionID> users_in_room;
+		{
+			std::lock_guard<std::mutex> lock(room_mutex);
+			users_in_room = rooms[room_id];
+		}
+
+		// for each user in the room
+		for (const auto & user : users_in_room)
+			// if the user is not the sender
+			if (user != message.id)
+				// send a new message
+				output_queue.put(Message(user, message.data));
+	}
 }
 
 void Server::listen_for_new_users()
@@ -19,17 +50,51 @@ void Server::listen_for_new_users()
 
 	for (;;)
 	{
-		Connection connection = connection_listener.wait_for_connection();
+		// get the next new connection
+		connection_ptr connection = connection_listener.wait_for_connection();
 
-		std::thread(&Server::read_from_connection, this, connection).detach();
+		// save the user
+		{
+			std::lock_guard<std::mutex> lock(users_mutex);
+			users[connection->get_id()] = make_pair(connection, "main");
+		}
+
+		{
+			std::lock_guard<std::mutex> lock(room_mutex);
+
+			// make sure the main room exists
+			if (rooms.find("main") == rooms.cend())
+				rooms["main"] = std::set<pipedat::ConnectionID>();
+
+			// add the user to the main room
+			rooms["main"].insert(connection->get_id());
+		}
+
+		// start a thread to listen to this user
+		std::thread(&Server::receive, this, connection).detach();
 	}
 
 }
 
-void Server::read_from_connection(Connection connection)
+void Server::receive(connection_ptr connection)
 {
 	for (;;)
 	{
-		const std::string message = connection.receive();
+		const std::string data = connection->receive();
+
+		if (data.size() == 0) return;
+
+		input_queue.put(Message(connection->get_id(), data));
+	}
+}
+
+void Server::send()
+{
+	for (;;)
+	{
+		// get the next message on the queue
+		const Message message = output_queue.get();
+		// send the message
+		users[message.id].first->send(message.data);
 	}
 }
--- Server/server.h
+++ Server/server.h
@@ -7,16 +7,32 @@
 #include <string>
 #include <mutex>
 
+#include "threadsafe_queue.h"
+
+class Message
+{
+public:
+	pipedat::ConnectionID id;
+	std::string data;
+
+	Message(const pipedat::ConnectionID & set_id, const std::string & set_data) : id(set_id), data(set_data) {}
+};
+
 class Server
 {
 private:
-	// map a username to a Connection
-	std::map<std::string, pipedat::Connection> users;
+	using connection_ptr = std::shared_ptr<pipedat::Connection>;
+	using user_data = std::pair<connection_ptr, std::string>;
+
+	// map a username to a Connection and a room name
+	std::map<pipedat::ConnectionID, user_data> users;
 	std::mutex users_mutex;
-	
+
 	// map a room to its users
-	std::map<std::string, std::set<std::string>> rooms;
+	std::map<std::string, std::set<pipedat::ConnectionID>> rooms;
 	std::mutex room_mutex;
+
+	threadsafe::queue<Message> input_queue, output_queue;
 
 public:
 	Server();
@@ -26,5 +42,7 @@ public:
 private:
 	void listen_for_new_users();
 
-	void read_from_connection(pipedat::Connection connection);
+	void receive(connection_ptr connection);
+
+	void send();
 };
