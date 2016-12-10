deb973eed7778263bdac11ebcd1a0be356b47b7c
Jim Viebke <jviebke@gmail.com>
2016-12-09 21:55:47 -0500

    add memory leak checks on the server

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,8 +1,24 @@
 
+#if defined(_DEBUG)
+#include <stdlib.h>
+#include <crtdbg.h>
+#define CRTDBG_MAP_ALLOC
+#endif
+
 #include "server.h"
 
 int main()
 {
+#if defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
+#endif
+
 	Server server;
 	server.run();
 }
