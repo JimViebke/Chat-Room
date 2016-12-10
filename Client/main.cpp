2a4c4c389679c69ce0b25a9b03d76ecd88c174d5
Jim Viebke <jviebke@gmail.com>
2016-12-09 21:39:59 -0500

    add memory leak checks

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,3 +1,10 @@
+
+#if defined(_DEBUG)
+#include <stdlib.h>
+#include <crtdbg.h>
+#define CRTDBG_MAP_ALLOC
+#endif
+
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -7,6 +14,16 @@
 
 int main()
 {
+#if defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
+#endif
+
 	const std::string ip_and_port =  ConnectionWindow(4, 70).run();
 
 	if (ip_and_port == "") return EXIT_FAILURE;
